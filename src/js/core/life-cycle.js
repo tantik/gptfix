6222081cc9223b9a973caf54264b37ada09925ec
tanti <tanti_2301@hotmail.com>
2025-02-19 18:14:58 +0900

    11

--- src/js/core/life-cycle.js
+++ src/js/core/life-cycle.js
@@ -1,59 +1,21 @@
-import {Emitter} from './emitter';
+import Emitter from './emitter';
 
-const HOOK_EMITTER = new Emitter();
-let moduleId;
+class LifeCycle {
+  constructor() {
+    this.emitter = Emitter;
+  }
 
-/* Module decorator */
-export function Initiate(options = {}) {
-	// create moduleId
-	moduleId = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
+  on(event, callback) {
+    this.emitter.subscribe(value => {
+      if (value === event) {
+        callback();
+      }
+    });
+  }
 
-	return function (classPrototype) {
-
-		const classInstance = new classPrototype();
-		/*
-		* HOOK `PREPARE` - prepare module with decorators
-		* */
-		HOOK_EMITTER.emit(`$prepare.module#${moduleId}`, classInstance);
-		/*
-		* HOOK `OnView` - invoke onViewInit methods
-		* */
-		if (options.onViewInit) {
-			onViewIO(classInstance).observe(options.onViewInit);
-		}
-
-		// return `init` to bootstrap
-		return () => classInstance.init ? classInstance.init() : false;
-	};
+  emit(event) {
+    this.emitter.emit(event);
+  }
 }
 
-export function Listener(target, eventName) {
-	return function (classPrototype, propertyName) {
-
-		// wait HOOK `PREPARE`
-		HOOK_EMITTER.on(`$prepare.module#${moduleId}`, (instance) => {
-
-			// on HOOK `PREPARE` addListeners
-			target.addEventListener(eventName, instance[propertyName].bind(instance));
-
-			// unsubscribe of HOOK `PREPARE`
-			HOOK_EMITTER.off(`$prepare.module#${moduleId}`);
-		});
-	};
-}
-
-/*
-* OnView - IntersectionObserver init module method onViewInit
-* !Important - need polyfill for IntersectionObserver
-* */
-function onViewIO(classInstance) {
-	return new IntersectionObserver((entries, observer) => {
-		entries.forEach(entry => {
-
-			if (entry.isIntersecting) {
-				classInstance.onViewInit();
-				observer.disconnect();
-			}
-		});
-	});
-}
+export default new LifeCycle();
