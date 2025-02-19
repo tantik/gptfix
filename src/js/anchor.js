1fbafd777a6aa95ec9376a44d4096fbce60a86cd
tanti <tanti_2301@hotmail.com>
2025-02-19 18:11:02 +0900

    updatw

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -1,73 +1,30 @@
-import {Initiate, Bind, Listener} from './core';
+import smoothscroll from 'smoothscroll-polyfill';
+import { Initiate } from './core';
 
-const d = document;
-const ls = localStorage;
-const LOCAL_STORAGE_ID = 'anchorId';
-
-const scrollToTop = () => window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-const scrollIntoView = ($$) => {
-	window.scrollTo({
-		top: $$.getBoundingClientRect().top + window.pageYOffset,
-		left: 0,
-		behavior: 'smooth'
-	});
-};
+// Инициализация smoothscroll-polyfill
+smoothscroll.polyfill();
 
 @Initiate()
 export class Anchor {
+  
+  constructor() {
+    this.links = document.querySelectorAll('a[href^="#"]');
+    this.addEventListeners();
+  }
 
-	@Bind('loadTrigger') pageLoaded = false;
-	@Bind('loadTrigger') polyfillLoaded = false;
-	needPolyfill = !('scrollBehavior' in d.documentElement.style);
+  addEventListeners() {
+    this.links.forEach(link => {
+      link.addEventListener('click', this.scrollToTarget.bind(this));
+    });
+  }
 
-	constructor() {
-		this.loadPolyfill();
-	}
-
-	@Listener(window, 'load')
-	load() {
-		this.pageLoaded = true;
-	}
-
-	loadPolyfill() {
-		if (this.needPolyfill) {
-			// load polyfill
-			import(/* webpackChunkName: "smoothscroll-polyfill" */ 'smoothscroll-polyfill').then((module) => {
-				this.polyfillLoaded = true;
-				module.polyfill();
-			});
-		}
-	}
-
-	loadTrigger() {
-		const isReady = this.pageLoaded && (this.polyfillLoaded || !this.needPolyfill);
-
-		if (isReady) {
-			const id = ls.getItem(LOCAL_STORAGE_ID);
-			if (id) {
-				// remove item and scrollIntoView
-				ls.removeItem(LOCAL_STORAGE_ID);
-				setTimeout(() => scrollIntoView(d.getElementById(id)), 300);
-			}
-		}
-	}
-
-	@Listener(d, 'click')
-	handleEvent(event) {
-		const $target = event.target.closest('[data-anchor]');
-		if (!$target) return;
-
-		const data = $target.getAttribute('data-anchor');
-
-		if ($target.href) {
-			// save to ls
-			event.preventDefault();
-			ls.setItem(LOCAL_STORAGE_ID, data);
-			window.location.assign($target.href);
-		} else {
-			// jumping
-			data === '' ? scrollToTop() : scrollIntoView(d.getElementById(data));
-		}
-	}
-
+  scrollToTarget(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
--- src/js/core/emitter.js
+++ src/js/core/emitter.js
@@ -1,31 +1,17 @@
 import { Subject } from 'rxjs';
 
-const createName = (name) => `$${ name }`;
+class Emitter {
+  constructor() {
+    this.subject = new Subject();
+  }
 
-export class Emitter {
+  emit(value) {
+    this.subject.next(value);
+  }
 
-	constructor() {
-		this.subjects = {};
-	}
-
-	emit( name, data ) {
-		const fnName = createName( name );
-		this.subjects[ fnName ] || ( this.subjects[ fnName ] = new Subject() );
-		this.subjects[ fnName ].next( data );
-	}
-
-	on( name, handler ) {
-		const fnName = createName( name );
-		this.subjects[ fnName ] || ( this.subjects[ fnName ] = new Subject() );
-		this.subjects[ fnName ].subscribe( handler );
-	}
-
-	off( name ) {
-		const fnName = createName( name );
-		if ( this.subjects[ fnName ] ) {
-			this.subjects[ fnName ].unsubscribe();
-			delete this.subjects[ fnName ];
-		}
-	}
-
+  subscribe(callback) {
+    return this.subject.subscribe(callback);
+  }
 }
+
+export default new Emitter();
